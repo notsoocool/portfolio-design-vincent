2dbf922a541c6843decace21139b66da7a111122
Yajush Vyas <dryashhoney@gmail.com>
2022-02-14 18:11:28 +0530

    change dark mode button

--- src/props/Header2.tsx
+++ src/props/Header2.tsx
@@ -26,9 +26,13 @@ const Header2 = () => {
             <button onClick={() => {
                 setColorMode(colorMode === ColorMode.dark ? ColorMode.light : ColorMode.dark)
                 }}>{colorMode === ColorMode.dark ? '' : ''}
-                <svg width="32" height="26" viewBox="0 0 32 26"  className='fill-transparent dark:fill-white w-10'>
-                    <circle cx="16" cy="13" r="9.5" className=' stroke-black dark:stroke-white duration-500 ease-in-out'/>
-                    <path fillRule="evenodd" clipRule="evenodd" d="M16 3V23C21.5228 23 26 18.5228 26 13C26 7.47715 21.5228 3 16 3Z" className='fill-black dark:fill-black'/>
+                <svg width="20" height="20" viewBox="0 0 20 20" className='hidden dark:block fill-transparent w-10 duration-500 ease-in-out'>
+                    <circle cx="10" cy="10" r="9.5" transform="rotate(-180 10 10)" stroke="white"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M10 20L10 8.74228e-07C4.47715 3.91405e-07 1.35705e-06 4.47715 8.74228e-07 10C3.91405e-07 15.5228 4.47715 20 10 20Z" fill="white"/>
+                </svg>
+                <svg width="32" height="26" viewBox="0 0 32 26"  className='block dark:hidden fill-transparent w-10 duration-500 ease-in-out'>
+                    <circle cx="16" cy="13" r="9.5" className=' stroke-black duration-500 ease-in-out'/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M16 3V23C21.5228 23 26 18.5228 26 13C26 7.47715 21.5228 3 16 3Z" className='fill-black'/>
                 </svg>
             </button>
             <svg width="32" height="26" viewBox="0 0 32 26" fill="none" xmlns="http://www.w3.org/2000/svg">
